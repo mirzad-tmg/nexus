644332ef14bff8d95b20ea3bbac5f446e574dd78
EJ Ciramella <eciramella@gmail.com>
2012-07-12 13:12:07 -0700

    Adding several working recipes now for setting up the reverse proxy.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -8,6 +8,23 @@ end
 
 
 link "/etc/nginx/sites-enabled/nexus" do
-  to "/etc/nginxi/sites-available/nexus"
+  to "/etc/nginx/sites-available/nexus"
   not_if "test -f /etc/nginx/sites-enabled/nexus"
 end
+#this seems like a pretty ghetto way to do this.  I'd think that during 
+#initial gninx setup, there should be some options to establish the default
+#site.  Does anyone want that?
+link "/etc/nginx/sites-enabled/default" do
+  action :delete
+  only_if "test -L /etc/nginx/sites-enabled/default"
+end
+
+link "/etc/nginx/sites-enabled/000-default" do
+  action :delete
+  only_if "test -L /etc/nginx/sites-enabled/000-default"
+end
+
+service "nginx" do
+  action :start
+end
+
